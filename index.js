3e27e0e3cadfaa2af91dcfd1d438592697c2c3f3
drakenevadie19 <drakennievadje19@gmail.com>
2024-07-12 02:42:56 -0500

    New Change: adding endpoints, did not set up MongoDB yet

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+let logHistory = [];
 
+app.post("/api/users", (req, res) => {
+  // Push user with username from request body to MongoDB
+});
 
+app.post("/api/users/:_id/exercises", (req, res) => {
+  // Push exercise with id from request body to MongoDB
+});
 
+app.get("/api/users/:_id/logs", (req, res) => {
+  const userId = req.params._id;
+  // Log all info of that user!
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
