5b1b93fa46d2e197c58c23692d43a0aeaf1f4dc9
drakenevadie19 <drakennievadje19@gmail.com>
2024-07-12 03:59:48 -0500

    New Change: in endpoints, adding variable to get criteria gotten from inputs

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
+
+// use mongoose
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL)
+ .then(() => console.log('MongoDB connected'))
+ .catch(err => console.log(err));
 
 let logHistory = [];
 
 app.post("/api/users", (req, res) => {
   // Push user with username from request body to MongoDB
+  // Getting inputted username
+  const userName = req.body.username;
 });
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   // Push exercise with id from request body to MongoDB
+  // Getting inputted userId from input, but need to check it again
+  const userId = req.body._id;
+
+  // Getting inputted exercise description from post body
+  const exerciseDescription = req.body.description;
+
+  // Getting inputted exercise duration from post body
+  const exerciseDuration = req.body.duration;
+
+  // Getting inputted exercise date from post body
+  const exerciseDate = req.body.date;
+
 });
 
 app.get("/api/users/:_id/logs", (req, res) => {
