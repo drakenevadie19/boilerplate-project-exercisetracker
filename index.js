00f8e9876fb6df43e8a7e34b2c639383d4c27381
drakenevadie19 <drakennievadje19@gmail.com>
2024-07-12 18:24:19 -0500

    New Change: adding details of endpoint of POST user

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.static('public'));
+
+// Middleware to parse JSON bodies
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -17,13 +23,40 @@ mongoose.connect(process.env.MONGO_URL)
  .then(() => console.log('MongoDB connected'))
  .catch(err => console.log(err));
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  log: [
+    {
+      description: { type: String, required: true },
+      duration: { type: Number, required: true },
+      date: { type: Date, required: true }
+    }
+  ]
+});
+
+const UserMongoDB = mongoose.model('UserMongoDB', userSchema);
+
 let logHistory = [];
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   // Push user with username from request body to MongoDB
   // Getting inputted username
   const userName = req.body.username;
-});
+  
+  // Check if user name existed or not
+  const isExisted = await UserMongoDB.findOne({ username: userName });
+  // console.log(isExisted);
+  if (isExisted) {
+    res.send({ error: "Username existed, please choose another username" });
+  } else {
+    const newUser = new UserMongoDB({ username: userName, log:[] });
+    const result = await newUser.save();
+
+    const user = await UserMongoDB.findOne({ username: userName });
+    const id = user._id;
+    res.send({ username: userName, _id: id })
+  }
+}); 
 
 app.post("/api/users/:_id/exercises", (req, res) => {
   // Push exercise with id from request body to MongoDB
@@ -39,11 +72,14 @@ app.post("/api/users/:_id/exercises", (req, res) => {
   // Getting inputted exercise date from post body
   const exerciseDate = req.body.date;
 
+  // Checking whether id existed in db or not, if not, send an error
+  // if yes, push to Mongo and send the inputted message
+  // if ()
 });
 
 app.get("/api/users/:_id/logs", (req, res) => {
   const userId = req.params._id;
-  // Log all info of that user!
+  // Log all info of that user
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
